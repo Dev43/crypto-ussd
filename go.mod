a6e7a70ecf53c731e5651f2dc7bffd55dfb79021
Dev43 <patrick.guay43@gmail.com>
2019-04-19 23:11:56 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/Dev43/crypto-ussd
+
+require (
+	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/radovskyb/watcher v1.0.6 // indirect
+)
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Post("/ussd", func(w http.ResponseWriter, r *http.Request) {
+		sessionId := r.FormValue("sessionId")
+		phoneNumber := r.FormValue("phoneNumber")
+		networkCode := r.FormValue("networkCode")
+		text := r.FormValue("text")
+
+		fmt.Println(sessionId, phoneNumber, networkCode, text)
+	})
+
+	port := ":3000"
+	fmt.Println("Listnening on port", port)
+	http.ListenAndServe(":3000", r)
+}
